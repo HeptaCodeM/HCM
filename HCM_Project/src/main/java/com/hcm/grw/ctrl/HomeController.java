7d070a196f302eab1c11e6f46606d756a0bc5184
jjang2121 <jjang2121@gmail.com>
2024-03-06 19:04:41 +0900

    20240306_190400_공통함수 추가

--- HCM_Project/src/main/java/com/hcm/grw/ctrl/HomeController.java
+++ HCM_Project/src/main/java/com/hcm/grw/ctrl/HomeController.java
@@ -10,54 +10,77 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hcm.grw.comm.CookiesMgr;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HomeController {
 
 	@GetMapping("/")
 	public void index() {
-		
+
 	}
-	
+
 	@GetMapping("/main.do")
 	public String main() {
 		return "main";
 	}
-	
+
 	@GetMapping("/Error404.do")
 	@ResponseBody
-	public void Error404(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void Error404(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		request.getRequestDispatcher("/WEB-INF/error/Error404.jsp").forward(request, response);
 	}
-	
+
 	@GetMapping("/Error500.do")
 	@ResponseBody
-	public void Error500(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void Error500(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		request.getRequestDispatcher("/WEB-INF/error/Error500.jsp").forward(request, response);
 	}
-	
+
 	@GetMapping("/login.do")
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/mainTmp.do")
 	public String mainTmp() {
 		return "mainTmp";
 	}
-	
+
 	@GetMapping("/hr/hrMainTmp.do")
 	public String hrMainTmp() {
 		return "hr/hrMainTmp";
 	}
-	
+
 	@GetMapping("/sm/smMainTmp.do")
 	public String smMainTmp() {
 		return "sm/smMainTmp";
 	}
-	
+
 	@GetMapping("/doc/docMainTmp.do")
 	public String docMainTmp() {
 		return "doc/docMainTmp";
+	}
+
+	@GetMapping("/setCookies.do")
+	public String cookiesTest(HttpServletResponse resp) {
+		
+		CookiesMgr.setCookies(resp, "testCk", "1111");
+		
+		return "redirect:/";
+	}
+
+	@GetMapping("/getCookies.do")
+	public String cookiesTest(HttpServletRequest req) {
+		
+		log.info(CookiesMgr.getCookies(req, "testCk"));
+		
+		return "redirect:/";
 	}
 	
 	
