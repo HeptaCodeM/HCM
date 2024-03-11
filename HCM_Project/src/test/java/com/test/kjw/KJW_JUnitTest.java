6a33d4aa13a71e9e86cc0a1ebdafad8c1f85de6f
kjwkjw02160 <kjwkjw02160@naver.com>
2024-03-11 11:39:37 +0900

    [김재원] 2024-03-11 작업분1

--- HCM_Project/src/test/java/com/test/kjw/KJW_JUnitTest.java
+++ HCM_Project/src/test/java/com/test/kjw/KJW_JUnitTest.java
@@ -53,125 +53,7 @@ public class KJW_JUnitTest {
 		assertNotNull(sessionTemplate);
 	}
 	
-	//@Test
-	public void testDept() {
-		List<CommonCodeDto> list = codeService.selectAllDept();
-		System.out.println(list);
-		assertNotNull(list);
-		
-		
-//		Map<String, Object> map = new HashMap<String, Object>();
-//		map.put("coco_cd", "DT000005");
-//		map.put("coco_name", "개발팀");
-//		map.put("coco_create_id", "SYSTEM");
-//		int cnt = codeService.insertDeptOne(map);
-//		assertEquals(1, cnt);
-		
-		
-//		Map<String, Object> map = new HashMap<String, Object>();
-//		map.put("coco_cd", "DT000005");
-//		int cnt = codeService.deleteDeptOne(map);
-//		assertEquals(1, cnt);
-
-//		List<CommonCodeDto> insertAfterlist = codeService.selectAllDept();
-//		System.out.println(insertAfterlist);
-		
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("coco_cd", "DT000004");
-		CommonCodeDto dto = codeService.selectOneDept(map);
-		System.out.println(dto);
-		assertNotNull(dto);
-		
-		
-//		Map<String, Object> map2 = new HashMap<String, Object>();
-//		map2.put("coco_cd", "DT000004");
-//		map2.put("coco_name", "기획팀");
-//		int cnt = codeService.correctionDept(map2);
-//		assertEquals(1, cnt);
-		
-		CommonCodeDto afterDto = codeService.selectOneDept(map);
-		System.out.println(afterDto);
-		
-	}
 	
-	//@Test
-	public void testRank() {
-		System.out.println("===================================================================");
-		List<CommonCodeDto> list = codeService.selectAllRank();
-		System.out.println(list);
-		assertNotNull(list);
-		System.out.println("===================================================================");
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("coco_cd", "RK000011");
-		map.put("coco_name", "연구원");
-		map.put("coco_create_id", "SYSTEM");
-		int cnt = codeService.insertRankOne(map);
-		assertEquals(1, cnt);
-		System.out.println("===================================================================");		
-		Map<String, Object> map2 = new HashMap<String, Object>();
-		map2.put("coco_cd", "RK000011");
-		CommonCodeDto dto = codeService.selectOneRank(map2);
-		System.out.println(dto);
-		assertNotNull(dto);
-		System.out.println("===================================================================");
-		Map<String, Object> map3 = new HashMap<String, Object>();
-		map3.put("coco_cd", "RK000011");
-		map3.put("coco_name", "수정테스트");
-		int cnt2 = codeService.correctionRank(map3);	
-		assertEquals(1, cnt2);
-		System.out.println("===================================================================");
-		Map<String, Object> map4 = new HashMap<String, Object>();
-		map4.put("coco_cd", "RK000011");
-		CommonCodeDto dto2 = codeService.selectOneRank(map4);
-		System.out.println(dto2);
-		System.out.println("===================================================================");
-		int cnt3 = codeService.deleteRankOne(map4);
-		assertEquals(1, cnt3);
-		System.out.println("===================================================================");
-		List<CommonCodeDto> list2 = codeService.selectAllRank();
-		System.out.println(list2);
-		
-	}
-	
-	//@Test
-	public void testPostion() {
-		System.out.println("===================================================================");
-		List<CommonCodeDto> list = codeService.selectAllPosition();
-		System.out.println(list);
-		assertNotNull(list);
-		System.out.println("===================================================================");
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("coco_cd", "PN000005");
-		map.put("coco_name", "파트장");
-		map.put("coco_create_id", "SYSTEM");
-		int cnt = codeService.insertPositionOne(map);
-		assertEquals(1, cnt);
-		System.out.println("===================================================================");		
-		Map<String, Object> map2 = new HashMap<String, Object>();
-		map2.put("coco_cd", "PN000005");
-		CommonCodeDto dto = codeService.selectOnePosition(map2);
-		System.out.println(dto);
-		assertNotNull(dto);
-		System.out.println("===================================================================");
-		Map<String, Object> map3 = new HashMap<String, Object>();
-		map3.put("coco_cd", "PN000005");
-		map3.put("coco_name", "수정테스트");
-		int cnt2 = codeService.correctionPosition(map3);	
-		assertEquals(1, cnt2);
-		System.out.println("===================================================================");
-		Map<String, Object> map4 = new HashMap<String, Object>();
-		map4.put("coco_cd", "PN000005");
-		CommonCodeDto dto2 = codeService.selectOnePosition(map4);
-		System.out.println(dto2);
-		System.out.println("===================================================================");
-		int cnt3 = codeService.deletePositionOne(map4);
-		assertEquals(1, cnt3);
-		System.out.println("===================================================================");
-		List<CommonCodeDto> list2 = codeService.selectAllPosition();
-		System.out.println(list2);
-		
-	}
 	
 	//@Test
 	public void testSign() {	
@@ -246,8 +128,8 @@ public class KJW_JUnitTest {
 		map2.put("comp_modify_id", "SYSTEM");
 		map2.put("comp_id", "ITCOM0A1");
 		
-		int cnt2 = companyService.correctionCompanyInfo(map2);
-		assertEquals(1, cnt2);
+//		int cnt2 = companyService.correctionCompanyInfo(map2);
+//		assertEquals(1, cnt2);
 		
 		CompanyDto dto2 = companyService.showCompanyInfo(map3);
 		System.out.println(dto2);
