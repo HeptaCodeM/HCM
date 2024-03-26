ebfb772e59730bc6435d97c2431126ab88cf20e6
kjwkjw02160 <kjwkjw02160@naver.com>
2024-03-26 12:36:48 +0900

    [김재원] 수정1

--- HCM_Project/src/main/java/com/hcm/grw/config/HttpSessionInitializer.java
+++ HCM_Project/src/main/java/com/hcm/grw/config/HttpSessionInitializer.java
@@ -1,11 +1,9 @@
-/*
- * package com.hcm.grw.config;
- * 
- * import org.springframework.session.web.context.
- * AbstractHttpSessionApplicationInitializer;
- * 
- * public class HttpSessionInitializer extends
- * AbstractHttpSessionApplicationInitializer {
- * 
- * }
- */
+ package com.hcm.grw.config;
+ 
+ import org.springframework.session.web.context.
+ AbstractHttpSessionApplicationInitializer;
+ 
+ public class HttpSessionInitializer extends
+ AbstractHttpSessionApplicationInitializer {
+ 
+ }
